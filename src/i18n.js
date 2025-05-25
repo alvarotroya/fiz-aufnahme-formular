708e8ff462444318ed5c1ab2c6e811e2e601f8b8
Álvaro Troya <alvaro.troya.ec@googlemail.com>
2025-05-25 19:58:55 +0200

    Migrate to vite

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import enPersonalInformation from './locales/en/personal_information.json';
+import dePersonalInformation from './locales/de/personal_information.json';
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
       en: {
-        personal_information: require('./locales/en/personal_information.json'),
+        personal_information: enPersonalInformation,
       },
       de: {
-        personal_information: require('./locales/de/personal_information.json'),
+        personal_information: dePersonalInformation,
       }
     },
-    lng: "en", // default language
-    fallbackLng: "en",
-    ns: ['personal_information'], // List namespaces to load
-    defaultNS: 'personal_information', // Default namespace
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    ns: ['personal_information'],
+    defaultNS: 'personal_information',
     interpolation: {
-      escapeValue: false
-    }
+      escapeValue: false,
+    },
   });
 
 export default i18n;
